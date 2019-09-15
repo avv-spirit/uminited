12d7ad955d553c0a7694c8b8b922cfb69a1c1800
avv-spirit <Sp1rit911@mail.ru>
2019-09-16 00:29:39 +0300

    Почти закончил с JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,38 @@
 'use strict';
 
-var butt_login = document.querySelector(".contacts-reg-buttons__login");
-var popup = document.querySelector(".login-popup");
-var close = popup.querySelector(".login-popup--modal-close");
+var buttLogin = document.querySelector('.contacts-reg-buttons__login');
+var popup = document.querySelector('.login-popup');
+var entireForm = document.querySelector('.login-popup__hide');
+var inputEmail = popup.querySelector('[name=popup-mail]');
 
-butt_login.addEventListener("click", function (evt) {
+
+buttLogin.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popup.classList.add("login-popup--show");
+  if (popup.classList.contains('login-popup') && entireForm.classList.contains('login-popup__hide')) {
+    popup.classList.remove('login-popup');
+    popup.classList.add('login-popup--show');
+    entireForm.classList.remove('login-popup__hide');
+    entireForm.classList.add('login-popup__open');
+  } else {
+    popup.classList.add('login-popup');
+    popup.classList.remove('login-popup--show');
+    entireForm.classList.add('login-popup__hide');
+    entireForm.classList.remove('login-popup__open');
+  }
+  inputEmail.focus();
 });
 
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("login-popup--show");
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("login-popup--show") && entireForm.classList.contains('login-popup__open')) {
+      popup.classList.remove("login-popup--show");
+      entireForm.classList.remove('login-popup__open');
+      entireForm.classList.add('login-popup__hide');
+    }
+  }
 });
+
+/*document.onclick = function() {
+  document.getElementById('login-form').style.display = 'none';
+};*/
