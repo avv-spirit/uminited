2baf5486d17d393316cd34e6c7688242c5b481fd
nor90 <adventure.rio@yandex.ru>
2019-09-25 21:08:51 +0300

    Scroll button!!!

--- source/js/jquery.js
+++ source/js/jquery.js
@@ -2,4 +2,18 @@ $(document).ready(function(){
   $('#btn-smart').click(function(){
     $('.smart-targeting__extratable-description').toggle();
   });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $('.scroll-btn').fadeIn();
+    } else {
+      $('.scroll-btn').fadeOut();
+    }
+  });
+  $('.scroll-btn').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
 });
